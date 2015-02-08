4477a82a5b0e7b3a8c868ddb02dee5d4bb6b3635
arbarnett <barnetal@onid.oregonstate.edu>
2015-02-08 14:00:31 -0700

    added the multi. table

--- multtable.php
+++ multtable.php
@@ -1,9 +1,4 @@
 <?php
-$min_multiplicand = htmlspecialchars($_GET["min-multiplicand"]);
-$max_multiplicand = htmlspecialchars($_GET["max-multiplicand"]);
-$min_multiplier = htmlspecialchars($_GET["min-multiplier"]);
-$max_multiplier = htmlspecialchars($_GET["max-multiplier"]);
-
 $errors = 0;
 
 if ($_GET["min-multiplier"] > $_GET["max-multiplier"]) {
@@ -20,8 +15,31 @@ foreach ($_GET as $key => $value) {
 	//source:http://stackoverflow.com/questions/19235746/php-check-to-see-if-variable-is-integer
 	if (!ctype_digit($value)) {
 		echo "$key must be an integer.<br/>";
+		$errors ++;
 	}
 }
 
+if ($errors === 0) {
+	//Source: http://www.dreamincode.net/forums/topic/219304-multiplication-table/
+
+	echo '<p> <h3> Multiplication Table </h3>';
+
+	echo '<table width="700" border="1" ';
+		echo '<tr><th></th>';
+			for($i = $_GET["min-multiplier"]; $i <= $_GET["max-multiplier"]; $i++) {
+				echo '<th>' . $i . '</th>';
+			}
+		echo '</tr>';
+
+		for ($i = $_GET["min-multiplicand"]; $i <= $_GET["max-multiplicand"]; $i++) {
+			echo '<tr><th>' . $i . '</th>';
+			for ($j = $_GET["min-multiplier"]; $j <= $_GET["max-multiplier"]; $j++) {
+				echo '<td>' . ($i * $j) . '</td>';
+			}
+			echo '</tr>';
+		}
+
+	echo '</table>';
+}
 
 ?>
