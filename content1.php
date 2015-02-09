fbe7ddd136e41d6c8f88ddb85ff01859e8c052ae
arbarnett <barnetal@onid.oregonstate.edu>
2015-02-09 13:39:45 -0700

    added greeting and user count to content1.php

--- content1.php
+++ content1.php
@@ -6,4 +6,25 @@ if(empty($_POST['username'])) {
 		echo " to return to the login screen.\n";
 	}
 
+if(!empty($_POST['username'])) {
+	session_start();
+
+	if(session_status() == PHP_SESSION_ACTIVE) {
+		if(isset($_POST['username'])) {
+			$_SESSION['username'] = $_POST['username'];
+		}
+		if(!isset($_SESSION['visits'])) {
+			$_SESSION['visits'] = 0;
+		}
+
+		echo "Hello $_SESSION[username]. You have visited this page $_SESSION[visits] times before.\n";
+
+		$_SESSION['visits']++;
+	
+		//this needs to also end the session
+		echo "Click ";
+		echo "<a href=http://web.engr.oregonstate.edu/~barnetal/login.php>here</a>";
+		echo " to logout.\n"
+	}
+}
 ?>
