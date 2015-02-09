fab2c0c35a8ed1426f3415710d18add1eeae1a86
arbarnett <barnetal@onid.oregonstate.edu>
2015-02-09 12:07:41 -0700

    corrected redirect if no username entered

--- content1.php
+++ content1.php
@@ -1,8 +1,9 @@
 <?php
 
 if(empty($_POST['username'])) {
-		echo "A username must be entered. Click here to return to the login screen \n";
-		echo "<form action ='http://web.engr.oregonstate.edu/~barnetal/login.php'><input type = 'submit' value ='Go back'</form>";
+		echo "A username must be entered. Click ";
+		echo "<a href=http://web.engr.oregonstate.edu/~barnetal/login.php>here</a>";
+		echo " to return to the login screen.\n";
 	}
 
 ?>
